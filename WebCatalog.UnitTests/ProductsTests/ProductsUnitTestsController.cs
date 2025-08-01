e2f98e07a177f180bd3ba3b5a873c16a6b78ec09
Emmanuel Egito Oliveira Candeira Marinho <e_egito@yahoo.com>
2025-08-01 06:58:31 -0300

    Adicionando testes

---
+++ WebCatalog.UnitTests/ProductsTests/ProductsUnitTestsController.cs
@@ -0,0 +1,12 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace WebCatalog.UnitTests.ProductsTests
+{
+    internal class ProductsUnitTestsController
+    {
+    }
+}
